8f10ccfebfada3ef975ecb5e9f29707878ba4ac2
Takuma Yamashita <c0b22154ba@edu.teu.ac.jp>
2023-06-13 16:46:59 +0900

    演習１まで

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,22 @@ def main():
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg") #練習1
     kk_img = pg.image.load("ex01-20230613/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 1, 1.0), pg.transform.rotozoom(kk_img, 2, 1.0), pg.transform.rotozoom(kk_img, 3, 1.0), pg.transform.rotozoom(kk_img, 4, 1.0), pg.transform.rotozoom(kk_img, 5, 1.0), pg.transform.rotozoom(kk_img, 6, 1.0), pg.transform.rotozoom(kk_img, 7, 1.0), pg.transform.rotozoom(kk_img, 8, 1.0), pg.transform.rotozoom(kk_img, 9, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0)]
     
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-(tmr % 1600), 0])
-        screen.blit(kk_imgs[tmr % 2], [300, 200])
+        kk = int(tmr / 5) % 20
+        if kk > 10:
+            kk = 20-kk
+        screen.blit(kk_imgs[kk], [300, 200])
 
         pg.display.update()
         tmr += 1       
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
