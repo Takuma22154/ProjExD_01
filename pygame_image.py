6a07741c46b37714861caaaa6eac11d5f240cd3b
Takuma Yamashita <c0b22154ba@edu.teu.ac.jp>
2023-06-13 15:35:54 +0900

    練習５まで

--- pygame_image.py
+++ pygame_image.py
@@ -8,9 +8,8 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg") #練習1
     kk_img = pg.image.load("ex01-20230613/fig/3.png")
-    kk_img_1 = pg.transform.flip(kk_img, True, False)
-    kk_img_2 = pg.transform.rotozoom(kk_img_1, 10, 1.0)
-    kk_imgs = [kk_img_1, kk_img_2]
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
