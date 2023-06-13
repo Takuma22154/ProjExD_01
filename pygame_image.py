6c113dc036902abf120e7c99b59d5995300ee010
Takuma Yamashita <c0b22154ba@edu.teu.ac.jp>
2023-06-13 15:32:41 +0900

    練習３

--- pygame_image.py
+++ pygame_image.py
@@ -8,14 +8,17 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg") #練習1
     kk_img = pg.image.load("ex01-20230613/fig/3.png")
-    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img_1 = pg.transform.flip(kk_img, True, False)
+    kk_img_2 = pg.transform.rotozoom(kk_img_1, 10, 1.0)
+    kk_imgs = [kk_img_1, kk_img_2]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
+
+        screen.blit(kk_imgs[tmr % 2], [300, 200])
 
         pg.display.update()
         tmr += 1        
