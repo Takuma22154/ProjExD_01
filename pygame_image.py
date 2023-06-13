6bdac4ad193370513f0ef406d0d023bb4f094038
Takuma Yamashita <c0b22154ba@edu.teu.ac.jp>
2023-06-13 16:04:31 +0900

    練習６まで

--- pygame_image.py
+++ pygame_image.py
@@ -6,21 +6,21 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+    tmr = 0
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg") #練習1
     kk_img = pg.image.load("ex01-20230613/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
-    tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-
+        screen.blit(bg_img, [-(tmr % 1600), 0])
         screen.blit(kk_imgs[tmr % 2], [300, 200])
 
         pg.display.update()
-        tmr += 1        
+        tmr += 1       
         clock.tick(10)
 
 
